6939ab42dc08a375ddfa7b00d03f5b2696063684
ali izadi <ali.izadi.m@gmail.com>
2019-07-31 12:21:36 +0430

    error and aop

---
+++ nodejs/user-management/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddUserComponent } from './add-user/add-user.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    AddUserComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ nodejs/user-management/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientService, User } from '../service/http-client.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  users:User[];
+
+  constructor(private httpClientService:HttpClientService) { }
+
+  ngOnInit() {
+    this.httpClientService.getUsers().subscribe(
+     response =>{this.users=response;}
+    );
+  }
+
+  deleteUser(user: User): void {
+    this.httpClientService.deleteUser(user)
+      .subscribe( data => {
+        this.users = this.users.filter(u => u !== user);
+      })
+  };
+}
