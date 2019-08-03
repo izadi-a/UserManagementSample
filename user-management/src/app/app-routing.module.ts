f18557c30389ade07f0a65b9633b63f5593858ea
ali izadi <ali.izadi.m@gmail.com>
2019-08-04 02:09:22 +0430

    angular

---
+++ user-management/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { AddUserComponent } from './add-user/add-user.component';
+
+const routes: Routes = [
+  { path:'', component: UserComponent},
+  { path:'adduser', component: AddUserComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
